07901891ac94cb8bbc54cddd281319c35ca8b292
Ricardo Mauro <ricardo.demauro@outlook.com>
2024-02-25 21:38:41 -0500

    initial version

--- RinhaBackend2024Q1/Program.cs
+++ RinhaBackend2024Q1/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using RinhaBackend2024Q1;
 using System.Net.Mime;
 using System.Text.Json;
 
@@ -8,7 +10,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHealthChecks()
     .AddMySql(connectionString: builder.Configuration.GetConnectionString("Default"));
 
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+});
+
 var app = builder.Build();
+
+DbService db = new DbService(builder.Configuration.GetConnectionString("Default"));
 
 app.MapGet("/", () => "Hello World!");
 
@@ -27,5 +36,21 @@ app.UseHealthChecks("/healthz", new HealthCheckOptions
         await context.Response.WriteAsync(result);
     }
 });
+
+app.MapPost("/clientes/{id}/transacoes",
+    async ([FromRoute] int id, [FromBody] TransacaoRequest transacaoRequest) =>
+    {
+        if (transacaoRequest?.IsValid() == true)
+        {
+            return await db.Transacao(id, transacaoRequest);
+        }
+        return Results.UnprocessableEntity();
+    });
+
+app.MapGet("/clientes/{id}/extrato",
+    async ([FromRoute] int id, CancellationToken cancellationToken) =>
+    {
+        return await db.Extrato(id, cancellationToken);
+    });
 
 app.Run();
