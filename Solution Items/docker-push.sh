f5e1088083626bcbf80e2d17ff79c28d6fc15d61
Ricardo <ricardo.demaruo@gmail.com>
2024-03-02 20:16:20 -0500

    adicionando scripts p deploy

--- Solution Items/docker-push.sh
+++ Solution Items/docker-push.sh
@@ -1,9 +1,9 @@
 tag=$(date +"%Y%m%d%H%M")
 
-docker buildx build --platform linux/amd64 -t rinha-2024q1-dotnet:$tag -t ricardomauro/rinha-2024q1-dotnet:latest .
+docker buildx build --platform linux/amd64 -t rinha-2024q1-dotnet-postgresql:$tag -t rinha-2024q1-dotnet-postgresql:latest .
 
-#docker build -t rinha-2024q1-dotnet:$tag -t ricardomauro/rinha-2024q1-dotnet:latest .
-docker push ricardomauro/rinha-2024q1-dotnet:$tag
-docker push ricardomauro/rinha-2024q1-dotnet:latest
+#docker build -t rinha-2024q1-dotnet:$tag -t rinha-2024q1-dotnet-postgresql:latest .
+docker push rinha-2024q1-dotnet-postgresql:$tag
+docker push rinha-2024q1-dotnet-postgresql:latest
 
 echo $tag
